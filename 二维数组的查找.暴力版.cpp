ad51513ce28a7ff0ba54af5d22677d7ec035aedb
1233liangyufei <46639080+1233liangyufei@users.noreply.github.com>
2019-02-09 23:06:02 +0800

    Create 二维数组的查找.暴力版.cpp

---
+++ 二维数组的查找.暴力版.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        for(int i = 0;i<array.size();i++){
+            for(int j = 0;j<array[0].size();j++){
+                if(array[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
